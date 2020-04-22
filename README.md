58bad1dcda261d7ad561da8d50301fca7ed672d0
Trent Waddington <trent.waddington@gmail.com>
2020-04-22 20:46:38 +1000

    I'm basically just passive aggressively coding now. I shouldn't be surprised... it's a state government department. Who knows what poor bastard got assigned the task of updating this website. Whoever it is, thanks for the qg-cards, that was special. What's it like over there?

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 My COViD-19 data analysis
 
 This is just amateur stuff, but if you find it useful, please use it.
+
+The data is scraped from <a href="https://www.qld.gov.au/health/conditions/health-alerts/coronavirus-covid-19/current-status/current-status-and-contact-tracing-alerts">a Queensland Health website</a> which is updated multiple times throughout the day, but conclusively around 4pm. This wouldn't be necessary if they just provided JSON, ya know?
+
+Producing qld.csv which is self explanatory.
+
+
--- csv.py
+++ csv.py
@@ -5,47 +5,64 @@ import calendar
 previous_total = None
 
 def process(timestamp, data):
-	confirmed_cases = data.find("confirmed cases")
-	if confirmed_cases == -1:
-		print(timestamp + " no data");
-		return
-	p = data.rfind("<p", 0, confirmed_cases)
-	pe = data.find(">", p)
-	endp = data.find("</p>", pe)
-	summary = data[pe+1:endp]
+	class_cases = data.find("class=\"cases\"")
+	if class_cases == -1:
+		confirmed_cases = data.find("confirmed cases")
+		if confirmed_cases == -1:
+			print(timestamp + " no data");
+			return
+		p = data.rfind("<p", 0, confirmed_cases)
+		pe = data.find(">", p)
+		endp = data.find("</p>", pe)
+		summary = data[pe+1:endp]
 
-	words = summary.split()
+		words = summary.split()
 
-	global previous_total
-	nums = []
-	for word in words:
-		if word[-1] == '.' or word[-1] == ",":
-			word = word[0:-1]
-		if word.isnumeric():
-			nums.append(word)
-	if len(nums) == 0:
-		if "no new confirmed cases" in summary:
-			total = previous_total
+		global previous_total
+		nums = []
+		for word in words:
+			if word[-1] == '.' or word[-1] == ",":
+				word = word[0:-1]
+			if word.isnumeric():
+				nums.append(word)
+		if len(nums) == 0:
+			if "no new confirmed cases" in summary:
+				total = previous_total
+			else:
+				total = summary
 		else:
-			total = summary
+			total = nums[-1]
+		previous_total = total
 	else:
-		total = nums[-1]
-	previous_total = total
+		dive = data.find(">", class_cases)	
+		endspan = data.find("</span>", dive)
+		cases = data[dive+1:endspan]
+		cases = cases.replace("<span>", "")
+		cases = cases.replace(",", "")
+		total = cases
 
 	th = data.find("table59454r1c2")
-	if th == -1:
-		total_samples_tested = data.find("Total samples tested")
-		if total_samples_tested == -1:
-			testing = "null"
-		else:
-			endp = data.find("</p>", total_samples_tested)
-			testing = data[total_samples_tested + len("Total samples tested:"):endp]
-	else:
+	total_samples_tested = data.find("Total samples tested")
+	if th != -1:
 		td = data.find("table59454r1c2", th + 1)
 		etd = data.find(">", td)
 		endtd = data.find("</td>", etd)
 		testing = data[etd+1:endtd]
-		
+			
+	elif total_samples_tested != -1:
+		endp = data.find("</p>", total_samples_tested)
+		testing = data[total_samples_tested + len("Total samples tested:"):endp]
+	else:
+		class_tested = data.find("class=\"tested\"")
+		if class_tested == -1:
+			testing = "no data"
+		else:
+			dive = data.find(">", class_tested)
+			endspan = data.find("</span>", dive)
+			tested = data[dive+1:endspan]
+			tested = tested.replace("<span>", "")
+			testing = tested
+
 	testing = testing.replace("<b>","")
 	testing = testing.replace("</b>","")
 	testing = testing.replace("<strong>","")
@@ -55,9 +72,17 @@ def process(timestamp, data):
 	if testing.find("\\xe2") != -1:
 		testing = testing[0:testing.find("\\xe2")]
 
+	lost = "null"
+	class_lost = data.find("class=\"lost\"")
+	if class_lost != -1:
+		dive = data.find(">", class_lost)
+		endspan = data.find("</span>", dive)
+		lost = data[dive+1:endspan]
+		lost = lost.replace("<span>", "")
+
 	month = calendar.month_name[int(timestamp[4:6])][:3]
 	day = int(timestamp[6:8])
-	print("\"" + month + " " + str(day) + "\", " + str(total) + ", " + str(testing))
+	print("\"" + month + " " + str(day) + "\", " + str(total) + ", " + str(testing) + ", " + str(lost))
 
 def main():
 	all_timestamps = []
